97007288778ec8e847aa29458ae7f7611e88d7a9
Tomas luquez <luquez1431@gmail.com>
2022-12-27 15:52:48 -0300

    boton

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ const spanHamburger = document.querySelector('.hamburger-box')
 const menues = document.querySelector('.cont-menues')
 const search = document.querySelector('.cont-search')
 
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        const whatsAlert = document.querySelector('.whatsAlert')
+        whatsAlert.style.display='none'
+    }, 4000);
+})
+
 document.addEventListener('click', (evento) => {
     if(evento.target.matches('.hamburger-inner') || evento.target.matches('.hamburger') || evento.target.matches('.hamburger-box')){
         hamburger.classList.toggle('is-active')
